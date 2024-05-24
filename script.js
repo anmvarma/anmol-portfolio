55357a33cdec1ef5bd3f796af698ef2bbc37aae9
Tanima Saxena <tanimasaxena@Tanimas-MacBook-Air.local>
2024-05-24 20:54:08 +0530

    Updated website design with Fluent Design System and glassy finish

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Message sent! Thank you for contacting, Anmol.');
+    alert('Message sent! Thank you for contacting Anmol.');
     this.reset();
 });
